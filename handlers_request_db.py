d4196cd63158f067aba4894008b7591a074d76bb
Vjacheslav <goplit2010.konovalov@yandex.ru>
2022-02-18 13:10:53 +0300

    add history

--- handlers_request_db.py
+++ handlers_request_db.py
@@ -1,6 +1,22 @@
 from requests_db import *
 
 
+def history_transaction_add(func):
+    def wrapper(*args, **qwargs):
+        result = func(*args, **qwargs)
+        user_id = result['user_id']
+        transaction_amount = result['transaction']
+        balance = result['balance']
+        try:
+            add_history_user(user_id, transaction_amount, balance)
+            print(f'В историю транзакций пользователся с id {user_id} успешно добавлена информация: изменен баланс на сумму {transaction_amount}')
+        except Exception as e:
+            print(f'Произошла ошибка записи в историю транзакция пользователся с id {user_id}')
+            print(e)
+        return result
+    return wrapper
+
+
 def handler_create_user(user_id):
     try:
         create_user(user_id)
@@ -9,40 +25,51 @@ def handler_create_user(user_id):
         return f'Пользователь c id {user_id} уже существует'
 
 
-def handler_user_info(user_id, all=False, balance=False,):
+def handler_user_info(user_id):
     try:
         if all:
-            user_info = get_user_info(user_id, all=all, balance=balance)
+            user_info = get_user_info(user_id)
             if user_info:
-                return user_info
+                return user_info[0]
             else:
                 return f'Пользователь с id {user_id}, не создан'
-        elif balance:
-            user_balance = get_user_info(user_id, balance=balance)[0]['balance']
-            if user_balance:
-                return float(user_balance)
-            else:
-                return f'У пользователя с id {user_id}, не сформирован счет'
     except Exception as e:
             return f'При запросе произошла ошибка: {e}'
 
-
+@history_transaction_add
 def handler_transactions(user_id, number, enrollment=False, write_down=False):
+    number = float(number)
+    balance = float(handler_user_info(user_id)['balance'])
+    if balance is None:
+        balance = 0.0
     try:
-        balance = handler_user_info(user_id, balance=True)
         if enrollment:
-            new_balance = balance + float(number)
+            new_balance = balance + number
             enrollment_and_write_downs(user_id, new_balance)
-            return f'Успешно пополнен баланс пользователя с id {user_id}, на сумму({number})'
+            transaction = number
+            balance = new_balance
+            message = f'Успешно пополнен баланс пользователя с id {user_id}, на сумму({number})'
         elif write_down:
             if balance - number <0:
-                return f'У пользователся с id {user_id} недостаточно средств на балансе, для списания суммы'
+                transaction = 0
+                message = f'У пользователся с id {user_id} недостаточно средств на балансе, для списания суммы'
             else:
-                new_balance = balance - float(number)
+                new_balance = balance - number
                 enrollment_and_write_downs(user_id, new_balance)
-                return f'У пользователя с id {user_id} успешно списана сумма({number}) с баланса'
+                transaction = -number
+                balance = new_balance
+                message = f'У пользователя с id {user_id} успешно списана сумма({number}) с баланса'
     except Exception as e:
-        return f'При транзакции произошла ошибка: {e}'
+        message = f'При транзакции произошла ошибка: {e}'
+    finally:
+        response = {
+            'user_id' : user_id,
+            'balance' : balance,
+            'transaction' : transaction,
+            'message' : message,
+            }
+        return response
 
-# print(handler_transactions(2, 600.25, enrollment=True))
-# print(handler_user_info(2, True))
+# handler_create_user(2)
+print(handler_transactions(2, 200, write_down=True))
+print(handler_user_info(2))
--- requests_db.py
+++ requests_db.py
@@ -16,7 +16,7 @@ connection = pymysql.connect(host=HOST,
                              cursorclass=pymysql.cursors.DictCursor)
 
 
-def create_user(user_id, balance=None, history=None, connection=connection):
+def create_user(user_id, connection=connection):
     with connection.cursor() as cursor:
         connection.ping()
         cursor.execute(f"INSERT INTO users_balance (user_id) VALUES ('{user_id}');")
@@ -24,26 +24,40 @@ def create_user(user_id, balance=None, history=None, connection=connection):
         connection.close()
 
 
-def get_user_info(id=None,all=False, balance=False, connection=connection):
+def get_user_info(user_id, connection=connection):
     with connection.cursor() as cursor:
         connection.ping()
-        if all:
-            cursor.execute(f"SELECT * FROM users_balance WHERE user_id = {id};")
-            result = cursor.fetchall()
-        elif balance:
-            cursor.execute(f"SELECT balance FROM users_balance WHERE user_id = {id};")
-            result = cursor.fetchall()
+        cursor.execute(f"SELECT * FROM users_balance WHERE user_id = {user_id};")
+        result = cursor.fetchall()
         connection.commit()
         connection.close()
         return result
 
 
-def enrollment_and_write_downs(user_id, number):
+def enrollment_and_write_downs(user_id, number, connection=connection):
     with connection.cursor() as cursor:
         connection.ping()
         cursor.execute(f"UPDATE users_balance SET balance = '{number}' WHERE user_id = '{user_id}'")
         connection.commit()
         connection.close()
+
+
+def add_history_user(user_id, transaction, balance, connection=connection):
+    with connection.cursor() as cursor:
+        connection.ping()
+        cursor.execute(f"INSERT INTO transaction_history (user_id, transaction, balance) VALUES ('{user_id}', '{transaction}', '{balance}');")
+        connection.commit()
+        connection.close()
+
+
+def get_history_user(user_id, connection=connection):
+    with connection.cursor() as cursor:
+        connection.ping()
+        cursor.execute(f"SELECT * FROM transaction_history WHERE user_id = {user_id};")
+        result = cursor.fetchall()
+        connection.commit()
+        connection.close()
+        return result
 
 # print(get_user_info(2,True))
 # print(enrollment_and_write_downs(2, 400))
@@ -52,4 +66,5 @@ def enrollment_and_write_downs(user_id, number):
 # CREATE DATABASE Avito;
 # CREATE TABLE users (id INT PRIMARY KEY AUTO_INCREMENT, name VARCHAR(255) NOT NULL, surname VARCHAR(255) NOT NULL, patronymic VARCHAR(255) NOT NULL);
 # CREATE TABLE balance (id INT PRIMARY KEY AUTO_INCREMENT, balance NUMERIC, user_id INT, FOREIGN KEY (user_id) REFERENCES users(id));
-# CREATE TABLE users_balance (id INT PRIMARY KEY AUTO_INCREMENT, user_id INT UNIQUE ,  balance NUMERIC, history JSON);
+# CREATE TABLE users_balance (user_id INT PRIMARY KEY UNIQUE ,  balance DECIMAL(15,2));
+# CREATE TABLE transaction_history (id INT PRIMARY KEY AUTO_INCREMENT, user_id INT, data TIMESTAMP DEFAULT CURRENT_TIMESTAMP, transaction TEXT, balance DECIMAL(15,2), FOREIGN KEY (user_id)  REFERENCES users_balance (user_id));
