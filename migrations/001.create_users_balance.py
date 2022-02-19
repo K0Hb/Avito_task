f9f5f72285a8c816db26743eb8d6c5f44fece5fa
Vjacheslav <goplit2010.konovalov@yandex.ru>
2022-02-19 23:51:00 +0300

    add migrate

---
+++ migrations/001.create_users_balance.py
@@ -0,0 +1,6 @@
+from yoyo import step
+steps = [
+   step(
+       "CREATE TABLE users_balance (user_id INT PRIMARY KEY UNIQUE, balance DECIMAL(15,2));"
+   )
+]
---
+++ migrations/002.create_transaction_history.py
@@ -0,0 +1,6 @@
+from yoyo import step
+steps = [
+   step(
+       "CREATE TABLE transaction_history (id INT PRIMARY KEY AUTO_INCREMENT, user_id INT, data TIMESTAMP DEFAULT CURRENT_TIMESTAMP, amount DECIMAL(15,2), balance DECIMAL(15,2), purpose TEXT, FOREIGN KEY (user_id) REFERENCES users_balance (user_id) ON DELETE CASCADE);"
+   )
+]
